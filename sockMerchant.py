51e54ec262be7d5a36e4f3f9a81b482912388d93
Lax Pan <lakshmidurga62@gmail.com>
2023-04-18 11:59:37 -0400

    Create sockMerchant.py

---
+++ sockMerchant.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    countdict = {}
+    count = 0
+    for i in range(n):
+        if ar[i] in countdict:
+            count=count+1
+            del countdict[ar[i]]
+        else:
+            countdict[ar[i]]=1
+    return count
+            
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
